31a3f41a93874b34b8a9e4c0e5bcd7cb67b4391b
Dillon <dillon.notz@gmail.com>
2019-05-09 23:33:32 -0400

    Update config files with newly added packages

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,16 @@
-module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
-};
+const presets = [
+  [
+    "@babel/env",
+    {
+      targets: {
+        edge: "17",
+        firefox: "60",
+        chrome: "67",
+        safari: "11.1",
+      },
+      useBuiltIns: "usage",
+    },
+  ],
+];
+
+module.exports = { presets };
